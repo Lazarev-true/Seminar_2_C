26eeb3c7193016cc8f7b9b241799ca58d0ca4c81
Lazarev Konstantin <klik@example.com>
2022-08-23 15:13:12 +0300

    кратно 7 и 23

---
+++ Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿int start = 1;
+int stop = 1000001;
+int number = new Random().Next(start, stop);
+
+int a = number % 7;
+int b = number % 23;
+
+if(a == 0 && b ==0)
+{
+    Console.Write(number + " кратно 7 и 23");
+}
+
+else
+{
+    Console.Write(number + " не кратно 7 и 23");
+}
