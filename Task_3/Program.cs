efd528e9e876b40607552076289bd751294ccc58
Lazarev Konstantin <klik@example.com>
2022-08-23 15:06:31 +0300

    Выходной ли?

---
+++ Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите цифру дня недели");
+string weeknumber = Console.ReadLine();
+int number = Convert.ToInt32(weeknumber);
+
+if(number <= 7 && number > 0)
+{
+    if(number > 5)
+    {
+        Console.Write(number + " -> да");
+    }
+
+    else
+    {
+        Console.Write(number + " -> нет");
+    }
+}
+
+else
+{
+    Console.Write(number + " -> День недели от 1 до 7");
+}
