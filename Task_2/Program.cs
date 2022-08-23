7b586b5690dc650a5ee587be723a611f333fe31c
Lazarev Konstantin <klik@example.com>
2022-08-23 15:01:36 +0300

    3-я цифра

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿int start = 1;
+int stop = 65537;
+int number = new Random().Next(start, stop);
+
+Console.Write(number + " -> ");
+
+if(number < 100)
+{
+    Console.Write("третьей цифры нет");
+}
+
+string strnumber = Convert.ToString(number);
+
+Console.Write(strnumber[2]);
