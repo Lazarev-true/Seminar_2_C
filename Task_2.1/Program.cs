a58d7b254a4275e89a47dc7fdf072cdf84baf6c0
Lazarev Konstantin <klik@example.com>
2022-08-24 19:12:36 +0300

    Третья цифра

---
+++ Task_2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿int start = 1;
+int stop = 65537;
+int number = new Random().Next(start, stop);
+
+if(number < 100)
+{
+    Console.Write(number + " -> третьей цифры нет");
+}
+if(number >= 100 && number < 1000)
+{
+    int digit = number % 10;
+    Console.Write(number + " -> " + digit);
+}
+if(number >= 1000 && number < 10000)
+{
+    int digit = number / 10 % 10;
+    Console.Write(number + " -> " + digit);
+}
+if(number >= 10000 && number < stop)
+{
+    int digit = number / 100 % 10;
+    Console.Write(number + " -> " + digit);
+}
